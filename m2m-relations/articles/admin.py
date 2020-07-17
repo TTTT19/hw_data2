afc24e37b6bb5849f9d9e69930b3b140ff625f92
TTTT19 <t.a.kamalov@gmail.com>
2020-07-17 17:59:56 +0300

    first commit

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Article_Tag, Tag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tags_count = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            if form.cleaned_data["main_tag_for_article"]:
+                main_tags_count += 1
+
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+
+        if main_tags_count != 1:
+            raise ValidationError('Определите один главный тег')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Article_Tag
+    formset = RelationshipInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
---
+++ orm_migrations/school/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    subject = models.CharField(max_length=10, verbose_name='Предмет')
+
+    class Meta:
+        verbose_name = 'Учитель'
+        verbose_name_plural = 'Учителя'
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+
+    teacher = models.ManyToManyField(Teacher,
+                                     through='Student_Teacher',
+                                     through_fields=('student', 'teacher'), related_name='students',
+                                     )
+
+    group = models.CharField(max_length=10, verbose_name='Класс')
+
+    class Meta:
+        verbose_name = 'Ученик'
+        verbose_name_plural = 'Ученики'
+
+    def __str__(self):
+        return self.name
+
+class Student_Teacher(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
