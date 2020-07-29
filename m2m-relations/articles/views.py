4cdc00c416fed52dcaf91f8cee2a2c03cf69a05f
TTTT19 <t.a.kamalov@gmail.com>
2020-07-29 23:27:42 +0300

    change3

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -6,8 +6,9 @@ from articles.models import Article, Article_Tag
 
 def articles_list(request):
     template = 'articles/news.html'
-    object_list = Article.objects.all().prefetch_related('scopes')
-    context = {'object_list':object_list}
+    object_list = Article.objects.all()
+    object_list_2 = Article_Tag.objects.all()
+    context = {'object_list':object_list, 'object_list_2': object_list_2}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
